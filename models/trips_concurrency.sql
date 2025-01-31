be6e0930a6eb87f65f886b8022800b66a0cad5be
Marat <igralkin@gmail.com>
2025-01-31 09:41:49 +0000

    Add new macros

--- models/trips_concurrency.sql
+++ models/trips_concurrency.sql
@@ -5,51 +5,31 @@ unnest_cte as (
         unnest(array[started_at, finished_at]) as "timestamp",
         unnest(array[1, -1]) as increment
     from
-        {{ source("scooters_raw", "trips") }}
+        {{ source('scooters_raw', 'trips') }}
 ),
 sum_cte as (
-    -- Make timestamp unique, group increments, add initial concurrency from history
+    -- Make timestamp unique, group increments
     select
         "timestamp",
-        sum(increment) as increment,
-        true as preserve_row
+        sum(increment) as increment
     from
         unnest_cte
-    where
-    {% if is_incremental() %}
-        "timestamp" > (select max("timestamp") from {{ this }})
-    {% else %}
-        "timestamp" < (date '2023-06-01' + interval '7' hour) at time zone 'Europe/Moscow'
-    {% endif %}
     group by
         1
-    {% if is_incremental() %}
-    union all
-    select
-        "timestamp",
-        concurrency as increment,
-        false as preserve_row
-    from
-        {{ this }}
-    where
-        "timestamp" = (select max("timestamp") from {{ this }})
-    {% endif %}
 ),
 cumsum_cte as (
     -- Integrate increment to get concurrency
     select
         "timestamp",
-        sum(increment) over (order by "timestamp") as concurrency,
-        preserve_row
+        sum(increment) over (order by "timestamp") as concurrency
     from
         sum_cte
 )
 select
     "timestamp",
-    concurrency
+    concurrency,
+    {{ updated_at() }}
 from
     cumsum_cte
-where
-    preserve_row
 order by
     1
